691bdca08b26a86d2320cc01bf72f3382206d9a2
Ian Conery <ianconery@gmail.com>
2015-08-26 12:29:53 -0700

    initial behvior for items

---
+++ Assets/ItemController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D () {
+		Debug.Log ("I have collided!");
+		Destroy (this.gameObject);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,16 +3,18 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 1;
+	public float speed = 4;
+	private Rigidbody2D rb2d;
 	// Use this for initialization
 	void Start () {
-	
+		this.rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float horizontal = Input.GetAxis ("Horizontal") * Time.deltaTime * this.speed;
-		float vertical = Input.GetAxis ("Vertical") * Time.deltaTime * this.speed;
-		this.transform.Translate (horizontal, vertical, 0);
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+//		this.transform.Translate (horizontal, vertical, 0);
+		this.rb2d.velocity = this.speed * new Vector2 (horizontal, vertical);
 	}
 }
