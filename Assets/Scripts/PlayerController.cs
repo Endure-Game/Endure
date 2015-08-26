7b55653923d85397f2f62c520e71072bfe2d1e95
Ian Conery <ianconery@gmail.com>
2015-08-26 11:53:00 -0700

    inital character and movement closes #2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal") * Time.deltaTime * this.speed;
+		float vertical = Input.GetAxis ("Vertical") * Time.deltaTime * this.speed;
+		this.transform.Translate (horizontal, vertical, 0);
+	}
+}
