36d87ea0da6eae4c6587c762811dd5cc0b308455
David Beckley <beckl.ds@gmail.com>
2015-08-27 19:13:04 -0700

    Refactor

--- Assets/Scripts/WorldController.cs
+++ Assets/Scripts/WorldController.cs
@@ -26,6 +26,12 @@ public class WorldController : MonoBehaviour {
 		roomScript.SetupRoom ();
 	}
 
+	// TODO: make better
+	private bool cameraLock = false;
+
+	private float roomWidth;
+	private float roomHeight;
+
 	// Use this for initialization
 	void Start () {
 		this.player = PlayerController.instance;
@@ -39,30 +45,30 @@ public class WorldController : MonoBehaviour {
 //				Instantiate (rooms[0], new Vector3(i*width*scale, j*width*scale, 0), Quaternion.identity);
 //			}
 //		}
+
+		// TODO: actually calculate this
+		this.roomWidth = 32;
+		this.roomHeight = 32;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// TODO: separate parts out into helper functions to make it less messy and monolithic
+		if (!this.cameraLock) {
+			TrackPlayer ();
+			CheckForRoomChange ();
+		}
+	}
+
+	void TrackPlayer () {
 		float playerX = this.player.transform.position.x;
 		float playerY = this.player.transform.position.y;
-
-		float playerWidth = this.player.GetComponent<SpriteRenderer>().sprite.bounds.size.x * this.player.transform.localScale.x;
-		float playerHeight = this.player.GetComponent<SpriteRenderer>().sprite.bounds.size.y * this.player.transform.localScale.y;
 
 		float cameraX = this.camera.transform.position.x;
 		float cameraY = this.camera.transform.position.y;
 
-		GameObject nextRoom = rooms[0];
-		float width = nextRoom.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
-		float height = nextRoom.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
-		float scale = (float) nextRoom.transform.localScale.x;
-
-		float scaledWidth = width * scale;
-		float verticalCameraPadding = scaledWidth - this.camera.getWidth ();
-
-		float scaledHeight = height * scale;
-		float horizontalCameraPadding = scaledWidth - this.camera.getHeight ();
+		float verticalCameraPadding = this.roomWidth - this.camera.getWidth ();
+		float horizontalCameraPadding = this.roomHeight - this.camera.getHeight ();
 
 		// make camera track player
 
@@ -71,39 +77,47 @@ public class WorldController : MonoBehaviour {
 
 		// only move camera to match player if the camera isn't at room edges
 		if (playerX <= verticalCameraPadding - (verticalCameraPadding / 2) &&
-			playerX >= -verticalCameraPadding / 2) {
+		    playerX >= -verticalCameraPadding / 2) {
 			newCameraX = playerX;
 		}
 
 		if (playerY <= horizontalCameraPadding - (horizontalCameraPadding / 2) &&
-		//if (playerY <= (scaledWidth - this.camera.getHeight ()) / 2 &&
 		    playerY >= -horizontalCameraPadding / 2) {
 			newCameraY = playerY;
 		}
 
 		this.camera.transform.position = new Vector3 (newCameraX,
 		                                              newCameraY,
-		                                   			  this.camera.transform.position.z);
+		                                              this.camera.transform.position.z);
+	}
+
+	void CheckForRoomChange() {
+		float playerX = this.player.transform.position.x;
+		float playerY = this.player.transform.position.y;
+
+		float playerWidth = this.player.GetComponent<SpriteRenderer>().sprite.bounds.size.x * this.player.transform.localScale.x;
+		float playerHeight = this.player.GetComponent<SpriteRenderer>().sprite.bounds.size.y * this.player.transform.localScale.y;
 
 		// detect when player hits boundary
-		if (scaledWidth / 2 - playerX < playerWidth / 2) {
+		if (this.roomWidth / 2 - playerX < playerWidth / 2) {
 			// east
-			this.ChangeRoom(new Vector2(-1, 0));
-		} else if (scaledWidth / 2 + playerX < playerWidth / 2) {
+			this.ChangeRoom (new Vector2 (-1, 0));
+		} else if (this.roomWidth / 2 + playerX < playerWidth / 2) {
 			// west
-			this.ChangeRoom(new Vector2(1, 0));
-		} else if (scaledHeight / 2 - playerY < playerHeight / 2) {
+			this.ChangeRoom (new Vector2 (1, 0));
+		} else if (this.roomHeight / 2 - playerY < playerHeight / 2) {
 			// north
-			this.ChangeRoom(new Vector2(0, 1));
-		} else if (scaledHeight / 2 + playerY < playerHeight / 2) {
+			this.ChangeRoom (new Vector2 (0, 1));
+		} else if (this.roomHeight / 2 + playerY < playerHeight / 2) {
 			// south
-			this.ChangeRoom(new Vector2(0, -1));
+			this.ChangeRoom (new Vector2 (0, -1));
 		}
 	}
 
 	void ChangeRoom (Vector2 direction)
 	{
-		this.camera.transform.Translate (0.1f * direction);
+		this.cameraLock = true;
+		this.camera.transform.Translate (this.roomHeight * direction);
 		this.player.transform.position = new Vector2 (0, 0);
 	}
 }
