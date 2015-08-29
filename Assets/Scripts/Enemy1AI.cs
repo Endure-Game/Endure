f71026b016d316a63f062da02986b17a8702b9b1
Pavtran <Pavtran@gmail.com>
2015-08-29 13:23:28 -0700

    (feat) STarted work on AI

---
+++ Assets/Scripts/Enemy1AI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1AI : Enemy1Script {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D (Collider2D other){
+		if (other.tag == "Player") {
+			if(PlayerController.checkAggro (aggro, this.gameObject)){
+				print ("Enemy has aggroed onto the player!");
+				
+			} else {
+				print ("no aggro.... yet");
+			}
+		}
+	}
+	
+	void OnTriggerExit2D (Collider2D other){
+		if (other.tag == "Player") {
+			print ("AWWW SHIT PLAY RAN AWAY :(");
+		}
+	}
+
+
+	public void DoSomething (){
+		print ("doing something");
+	}
+}
--- Assets/Scripts/Enemy1Script.cs
+++ Assets/Scripts/Enemy1Script.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class Enemy1Script : MonoBehaviour {
 
-	private float aggro = (float) 0.5;
-
+	public float aggro = (float) 0.5;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void Awake () {
+
 	}
 	void OnTriggerEnter2D () {
 		//PlayerController.instance.IncrementCounter ();
@@ -21,13 +23,10 @@ public class Enemy1Script : MonoBehaviour {
 		//print ("Enemy has vision of player");
 	}
 
-	void OnTriggerStay2D (Collider2D other){
-		if (other.tag == "Player") {
-			if(PlayerController.checkAggro (this.aggro, this.gameObject)){
-				print ("Enemy has aggroed onto the player!");
-			} else {
-				print ("no aggro.... yet");
-			}
-		}
+
+	void Attack (){
+		print ("AM ATTACKING PLAYER AW SHIT NIGGA!");
 	}
+
+
 }
