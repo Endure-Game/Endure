acc78f3153a2d162c6e12d452a5103fdc892f698
Pavtran <Pavtran@gmail.com>
2015-08-29 16:14:32 -0700

    (feat) Continued work on EnemyAI Logic

--- Assets/Scripts/Enemy1AI.cs
+++ Assets/Scripts/Enemy1AI.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy1AI : Enemy1Script {
+public class Enemy1AI : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -13,25 +13,38 @@ public class Enemy1AI : Enemy1Script {
 	
 	}
 
-	void OnTriggerStay2D (Collider2D other){
-		if (other.tag == "Player") {
-			if(PlayerController.checkAggro (aggro, this.gameObject)){
-				print ("Enemy has aggroed onto the player!");
-				
-			} else {
-				print ("no aggro.... yet");
-			}
-		}
-	}
-	
-	void OnTriggerExit2D (Collider2D other){
-		if (other.tag == "Player") {
-			print ("AWWW SHIT PLAY RAN AWAY :(");
-		}
-	}
 
+	public void enemyActive (GameObject enemy, float speed){
+		print (speed);
+		print (enemy.transform.position.x);
 
-	public void DoSomething (){
-		print ("doing something");
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		float vertical = Input.GetAxisRaw ("Vertical");
+		
+		Vector2 playerSpeed = new Vector2 (horizontal, vertical);
+		float magnitude = playerSpeed.magnitude;
+		if(magnitude == 0){
+			magnitude = 1;
+		}
+		
+		
+		
+		this.rb2d.velocity = this.speed * (playerSpeed/magnitude);
+		
+		if (horizontal > 0) {
+			this.animator.SetInteger ("Direction", 3);
+			this.animator.SetBool ("Idle", false);
+		} else if (horizontal < 0) {
+			this.animator.SetInteger ("Direction", 1);
+			this.animator.SetBool ("Idle", false);
+		} else if (vertical > 0) {
+			this.animator.SetInteger ("Direction", 2);
+			this.animator.SetBool ("Idle", false);
+		} else if (vertical < 0) {
+			this.animator.SetInteger ("Direction", 0);
+			this.animator.SetBool ("Idle", false);
+		} else {
+			this.animator.SetBool ("Idle", true);
+		}
 	}
 }
--- Assets/Scripts/Enemy1Script.cs
+++ Assets/Scripts/Enemy1Script.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class Enemy1Script : MonoBehaviour {
 
-	public float aggro = (float) 0.5;
+	public float speed = 3;
+	private float aggro = (float) 1.0;
+	private static Enemy1AI enemyAI;
 
 	// Use this for initialization
 	void Start () {
+		enemyAI = this.gameObject.AddComponent <Enemy1AI> ();
+		print (enemyAI);
 	}
 	
 	// Update is called once per frame
@@ -18,15 +22,34 @@ public class Enemy1Script : MonoBehaviour {
 
 	}
 	void OnTriggerEnter2D () {
-		//PlayerController.instance.IncrementCounter ();
+		//PlayerController.instance.IncrementCounter ()
 		//Destroy (this.gameObject);
 		//print ("Enemy has vision of player");
 	}
 
+	void OnTriggerStay2D (Collider2D other){
+		if (other.tag == "Player") {
+			if(PlayerController.checkAggro (this.aggro, this.gameObject)){
+				//print ("Enemy has aggroed onto the player!");
+				
+				enemyAI.enemyActive (this.gameObject, this.speed);
+			} else {
+				print ("no aggro.... yet");
+			}
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other){
+		if (other.tag == "Player") {
+			print ("AWWW SHIT PLAY RAN AWAY :(");
+		}
+	}
 
 	void Attack (){
 		print ("AM ATTACKING PLAYER AW SHIT NIGGA!");
 	}
+
+
 
 
 }
