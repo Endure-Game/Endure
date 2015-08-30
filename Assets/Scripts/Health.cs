b6b6c66470704c8304f19405384e98438aa0de2c
David Beckley <beckl.ds@gmail.com>
2015-08-30 10:45:52 -0700

    Create HealthChange prefab on health change

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Health : MonoBehaviour {
 	public int maxHealth = 10;
 
+	private GameObject healthChangeDisplay;
+
 	private int currentHealth;
 	public int CurrentHealth {
 		get {
@@ -14,6 +16,8 @@ public class Health : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		this.currentHealth = this.maxHealth;
+		this.healthChangeDisplay = Resources.Load ("HealthChange", typeof(GameObject)) as GameObject;
+		print (this.healthChangeDisplay);
 	}
 
 	public void ChangeHealth (int delta) {
@@ -23,7 +27,11 @@ public class Health : MonoBehaviour {
 			delta = this.maxHealth - this.currentHealth;
 		}
 
+		Vector3 startPos = new Vector3 (this.transform.position.x, this.transform.position.y + 0.75f, 0);
+
 		// TODO: add health change animation
+		GameObject change = Instantiate (healthChangeDisplay, startPos, Quaternion.identity) as GameObject;
+		change.GetComponent<HealthAnimation> ().healthChange = delta;
 
 		this.currentHealth += delta;
 
