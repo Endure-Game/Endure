a6ae89f4602efccebfcab3cf6f300729ab13ba32
Pavtran <Pavtran@gmail.com>
2015-09-05 14:22:47 -0700

    (Feat) Refactored code for aggro

--- Assets/Scripts/Enemy1AI.cs
+++ Assets/Scripts/Enemy1AI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Enemy1AI : MonoBehaviour {
 	public float speed = 3f;
+	public float aggro = 2f;
 
 	private bool active = false;
 	private Rigidbody2D rb2d;
@@ -21,29 +22,35 @@ public class Enemy1AI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (true) {
+		if (this.active) {
 			Vector2 heading = player.transform.position - this.transform.position;
-
-			if (!melee.Locked) {
-				rb2d.velocity = speed * heading.normalized;
-			} else {
-				rb2d.velocity = Vector2.zero;
-			}
-
-			if (heading.magnitude < melee.range + 0.5f) {
-				Vector2 n = heading.normalized;
-				if (n.x > Mathf.Sqrt (2) / 2) {
-					melee.AttackEast ();
-				} else if (n.x < - Mathf.Sqrt (2) / 2) {
-					melee.AttackWest ();
-				} else if (n.y > Mathf.Sqrt (2) / 2) {
-					melee.AttackNorth ();
-				} else if (n.y < -Mathf.Sqrt (2) / 2) {
-					melee.AttackSouth ();
+			//print (heading.magnitude + "|" + this.aggro);
+			if(heading.magnitude < this.aggro){
+				if (!melee.Locked) {
+					this.rb2d.velocity = speed * heading.normalized;
+				} else {
+					this.rb2d.velocity = Vector2.zero;
 				}
+
+				if (heading.magnitude < melee.range + 0.5f) {
+					Vector2 n = heading.normalized;
+					if (n.x > Mathf.Sqrt (2) / 2) {
+						this.melee.AttackEast ();
+					} else if (n.x < - Mathf.Sqrt (2) / 2) {
+						this.melee.AttackWest ();
+					} else if (n.y > Mathf.Sqrt (2) / 2) {
+						this.melee.AttackNorth ();
+					} else if (n.y < -Mathf.Sqrt (2) / 2) {
+						this.melee.AttackSouth ();
+					}
+				}
+			} else {
+				//TODO RANDOM MOVEMENT
+				print("Stopped chasing player");
+				this.rb2d.velocity = Vector2.zero;
 			}
 		} else {
-			rb2d.velocity = Vector2.zero;
+			this.rb2d.velocity = Vector2.zero;
 		}
 
 		// Make sure enemy is on the right layer
@@ -59,6 +66,23 @@ public class Enemy1AI : MonoBehaviour {
 			transform.localScale = new Vector3(-1f, 1f, 1f);
 		} else {
 			animator.SetBool("moving", false);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D collided){
+		if(collided.tag == "Player"){
+			print ("Activated Enemy");
+			this.active = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D collided) {
+		CircleCollider2D playerCollider = player.GetComponent<CircleCollider2D> ();
+		Vector2 heading = player.transform.position - this.transform.position;
+		print ("collider radius: " + playerCollider.radius + " headingmag: " + heading.magnitude);
+		if (collided.tag == "Player" && heading.magnitude >= playerCollider.radius) {
+			print ("Deactivated Enemy");
+			this.active = false;
 		}
 	}
 
