b151ac71a31cbb87d5a2844a80d69f2218388947
David Beckley <beckl.ds@gmail.com>
2015-09-08 16:54:59 -0700

    Make character icon track player

---
+++ Assets/CharacterIcon.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterIcon : MonoBehaviour {
+	public GameObject world;
+	private RoomManager roomManager;
+	private RectTransform rt;
+
+	// Use this for initialization
+	void Start () {
+		this.roomManager = world.GetComponent<RoomManager> ();
+		this.rt = this.GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var roomWidth = this.roomManager.columns;
+		var roomHeight = this.roomManager.rows;
+
+		var newLocation = PlayerController.instance.transform.position;
+		newLocation += new Vector3 (roomWidth / 2, roomHeight / 2);
+
+		newLocation = Vector3.Scale (newLocation, new Vector3 (150f / (roomWidth * this.roomManager.roomSide), 150f / (roomHeight * this.roomManager.roomSide), 1));
+
+		//print ("heeey" + newLocation);
+
+		this.rt.anchoredPosition = newLocation;
+	}
+}
