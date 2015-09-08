0db8b3c3dc221852989443a553d8bd658606098d
knockoutdonkey <chrislockwood92@gmail.com>
2015-09-08 12:04:18 -0700

    Added plains that partialy cover up player and objects

--- Assets/Scripts/RoomGeneration/PlainsTile.cs
+++ Assets/Scripts/RoomGeneration/PlainsTile.cs
@@ -6,6 +6,8 @@ using Random = UnityEngine.Random;
 public class PlainsTile : BiomeTile
 {
 
+	public GameObject flatGroundTile;
+
 	// randomization constants
 	public int bloomNum = 100;
 	public RoomManager.Count bloomSize = new RoomManager.Count(3, 7);
@@ -14,6 +16,8 @@ public class PlainsTile : BiomeTile
 
 	public override void RandomBlocking(List<Tile> region) {
 
+		Destroy (this.tileHolder);
+		// Place elevation tiles
 		base.RandomBlocking(region);
 
 		for (int num = 0; num < bloomNum; num++) {
@@ -24,6 +28,29 @@ public class PlainsTile : BiomeTile
 			                  Random.Range (this.bloomSize.minimum, this.bloomSize.maximum + 1),
 			                  new TilePlacer(this.placeBlockingTile));
 		}
+	}
+
+	private GameObject tileHolder;
+	public override GameObject getGroundTile() {
+
+		if (this.tileHolder != null) {
+			Destroy (this.tileHolder);
+		}
+		this.tileHolder = new GameObject();
+
+		for (int i = 0; i < 4; i++) {
+			GameObject randomSprite = this.groundTiles[Random.Range(0, this.groundTiles.Length)];
+			GameObject tile = Instantiate(randomSprite,
+																		new Vector3(0f, (float)i / 4, (float)i / 4),
+		                               	Quaternion.identity) as GameObject;
+			tile.transform.parent = this.tileHolder.transform;
+		}
+
+		return this.tileHolder;
+	}
+
+	public GameObject getFlatGroundTile() {
+		return this.flatGroundTile;
 	}
 
 	public override int getBiomeNumber() {
