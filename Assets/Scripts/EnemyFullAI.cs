6a310ee087d9170d24b96e011a3e744f6eeb5c1c
Pavtran <Pavtran@gmail.com>
2015-09-09 12:00:00 -0700

    Started integrating pathfinding script

--- Assets/Scripts/EnemyFullAI.cs
+++ Assets/Scripts/EnemyFullAI.cs
@@ -20,7 +20,8 @@ public class EnemyFullAI : MonoBehaviour {
 	private Vector3 lastPlayerPos;
 	private Vector3 heading;//TODO FIGURE OUT WHY I HAVE THIS SHIT
 	private Vector3 targetHeading;
-	//private PathFinding pathFinding;// = GetComponent<PathFinding> ();
+	private PathFinding pathFinding;// = GetComponent<PathFinding> ();
+	private List<Vector3> path;
 	//For idle movement (Privately set)
 	private Animator animator;
 	private Vector3 oldPosition;
@@ -88,7 +89,7 @@ public class EnemyFullAI : MonoBehaviour {
 	}
 
 	void Awake (){
-		//this.pathFinding = GetComponent<PathFinding> ();
+		this.pathFinding = GetComponent<PathFinding> ();
 	}
 
 	// Update is called once per frame
@@ -139,9 +140,11 @@ public class EnemyFullAI : MonoBehaviour {
 	}
 	//moveTo should be a vector3 of position
 	public void MoveTo (Vector3 moveTo){
-		//List<Vector3> path =  this.pathFinding.PathFinder(this.transform.position, moveTo);
-		//this.heading = path[0] - this.transform.position;
-		this.heading = moveTo - this.transform.position;
+		if (this.path == null) {
+			this.path = this.pathFinding.PathFinder (this.transform.position, moveTo);
+		}
+		this.heading = path[0] - this.transform.position;
+		//this.heading = moveTo - this.transform.position;
 		this.rb2d.velocity = this.heading.normalized * this.speed;
 	}
 
