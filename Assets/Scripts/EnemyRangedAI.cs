1cf54a31c8d2ef07d8a38ca44c9c604664aacd82
knockoutdonkey <chrislockwood92@gmail.com>
2015-09-08 19:13:02 -0700

    Added north gun animation

--- Assets/Scripts/EnemyRangedAI.cs
+++ Assets/Scripts/EnemyRangedAI.cs
@@ -9,16 +9,18 @@ public class EnemyRangedAI : MonoBehaviour {
 	private bool active = false;
 	private Rigidbody2D rb2d;
 	private PlayerController player;
+	private Animator animator;
 	private RangedAttacker ranged;
 
-	
+
 	// Use this for initialization
 	void Start () {
 		this.player = PlayerController.instance;
 		this.rb2d = this.GetComponent<Rigidbody2D> ();
 		this.ranged = this.GetComponent<RangedAttacker> ();
+		this.animator = this.GetComponent<Animator> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (true) {
@@ -31,7 +33,7 @@ public class EnemyRangedAI : MonoBehaviour {
 				this.rb2d.velocity = Vector2.zero;
 			}
 			if (heading.magnitude < this.cowardDistance && !ranged.Locked){
-				this.rb2d.velocity = this.speed * -heading.normalized; 
+				this.rb2d.velocity = this.speed * -heading.normalized;
 			}
 			else if (heading.magnitude < this.rangedDistance){
 				//Vector3 target = heading;
@@ -43,14 +45,24 @@ public class EnemyRangedAI : MonoBehaviour {
 			this.rb2d.velocity = Vector2.zero;
 		}
 
-		
+
 		// Make sure enemy is on the right layer
-		this.transform.position = new Vector3(this.transform.position.x, 
-		                                      this.transform.position.y, 
+		this.transform.position = new Vector3(this.transform.position.x,
+		                                      this.transform.position.y,
 		                                      (float)(this.transform.position.y + 16));
+
+		if (this.rb2d.velocity.x > 0) {
+			animator.SetBool("moving", true);
+			transform.localScale = new Vector3(1f, 1f, 1f);
+		} else if (this.rb2d.velocity.x < 0) {
+			animator.SetBool("moving", true);
+			transform.localScale = new Vector3(-1f, 1f, 1f);
+		} else {
+			animator.SetBool("moving", false);
+		}
 	}
-	
-	
+
+
 	public void enemyActive (bool a){
 		this.active = a;
 	}
