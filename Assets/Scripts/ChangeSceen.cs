4f7645901cda666bb5644b6499988e78c6776867
Ian Conery <ianconery@gmail.com>
2015-09-13 16:58:23 -0700

    (feat) Improved building / it now calls the change screen script

--- Assets/Scripts/ChangeSceen.cs
+++ Assets/Scripts/ChangeSceen.cs
@@ -6,15 +6,19 @@ public class ChangeSceen : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
-
+	void OnTriggerEnter2D (){
+		print ("CHANGESCREEN HAS BEEN CALLED");
+//		Application.LoadLevelAsync (3);
+//		print("Total levels!!" + Application.levelCount); total count is 3 levels
+	}
 
 
 
