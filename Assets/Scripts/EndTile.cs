d30a6b1a41ccf42d457aa395aabbf3a25a7ad797
David Beckley <beckl.ds@gmail.com>
2015-09-16 11:22:51 -0700

    Add end tile
    It's a bit slow; I can fix that eventually

---
+++ Assets/Scripts/EndTile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndTile : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.collider.tag == "Player") {
+			Application.LoadLevel (4);
+		}
+	}
+}
