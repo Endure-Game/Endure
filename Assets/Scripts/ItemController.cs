5554ebd2f97e6987dfaafeba0cc6c31d12c2baa6
Pavtran <Pavtran@gmail.com>
2015-09-16 15:33:00 -0700

    Started adding collision ignore

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -9,7 +9,7 @@ public class ItemController : MonoBehaviour {
 	private float aggro = (float) 0.18;
 
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
@@ -23,6 +23,11 @@ public class ItemController : MonoBehaviour {
 		if (other.tag == "Player") {
 			Sounds.instance.Pickup ();
 			Destroy (this.gameObject);
+		} else {
+			if (!this.gameObject.GetComponent<Collider> ()) {
+				this.gameObject.AddComponent<Collider> ();
+				Physics.IgnoreCollision (this.GetComponent<Collider> (), GetComponent<Collider> ());		
+			}
 		}
 		//PlayerController.instance.IncrementCounter ();
 		//Destroy (this.gameObject);
