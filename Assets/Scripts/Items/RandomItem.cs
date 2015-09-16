6bc350ae3ea2ed50ca3a82e1c1c7d8e322fec6f1
knockoutdonkey <chrislockwood92@gmail.com>
2015-09-16 07:41:37 -0700

    Player can now only pick up one of a tool, otherwise gets 10 ammo

---
+++ Assets/Scripts/Items/RandomItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomItem : MonoBehaviour {
+
+  public GameObject[] weapons;
+  public GameObject[] tools;
+  public GameObject[] upgrades;
+
+  public static string[] placed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
